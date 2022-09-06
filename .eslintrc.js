3996923a4270c5236d548a24edc34fb74c3e04c0
胡小根 <hxg@haomo-studio.com>
2022-09-06 19:49:54 +0800

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off',
+    'vue/no-unused-components': 'off',
     'prettier/prettier': 0
     // 'prettier/prettier': [
     //   'error',
