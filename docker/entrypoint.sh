1ed8a4d11f3cdf1fa0b6fb01f5021c8a6244903a
胡小根 <hxg@haomo-studio.com>
2022-09-07 11:16:32 +0800

    chore: fix docker entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,7 @@ npm config set ignore-scripts false
 git add src/components/ . ':!src/components/built-in'
 git add src/pages && git add src/router && git commit -m "chore: restart and bak"
 git stash -u && git pull
-cp /root/code-live-preview/docker/client.js /root/code-live-preview/node_modules/vite/dist/client/client.js
+cp /root/code-live-preview/docker/client.mjs /root/code-live-preview/node_modules/vite/dist/client/client.mjs
 rm -rf /root/code-live-preview/node_modules/.vite/*.js.map
 #npm run serve
 node node_modules/vite/bin/vite.js
