f2b6bb69cc7bf154418ef755e92afd8bcee33a75
胡小根 <hxg@haomo-studio.com>
2022-09-13 19:48:53 +0800

    chore: update built-in-deploy.sh

--- built-in-deploy.sh
+++ built-in-deploy.sh
@@ -3,4 +3,4 @@ rsync -avz src/components/built-in haomo@192.168.1.7:/data/docker/design-cloud/f
 rsync -avz src/components/built-in haomo@192.168.1.7:/data/docker/design-cloud.stage/files/
 rsync -avz src/components/built-in haomo@192.168.1.7:/data/docker/design-cloud.dev/files/
 rsync -avz src/components/built-in haomo@192.168.1.8:/data/docker/design-cloud.dev/files/
-rsync -avz src/components/built-in ubuntu@159.75.226.181:/data/docker/design-cloud.prod/files/
+# rsync -avz src/components/built-in ubuntu@159.75.226.181:/data/docker/design-cloud.prod/files/
